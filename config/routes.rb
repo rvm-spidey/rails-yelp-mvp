fdca20dfa699ea605b660e3db311044502cedaaa
rishi monohur <rishi.monohur15@gmail.com>
2023-08-10 13:49:02 +0400

    4.4 A visitor can add a new review to a restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  # root "restaurants#index"
+  root "restaurants#index"
 
-  resources :restaurants, only: [:index, :create, :new, :show]
+  resources :restaurants, only: [:index, :create, :new, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
