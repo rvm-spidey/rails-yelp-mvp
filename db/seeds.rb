bba785de78f53929af54c4023e38a50e7286758c
rishi monohur <rishi.monohur15@gmail.com>
2023-08-10 10:55:17 +0400

    3 added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample(1)[0]
+  )
+  restaurant.save!
+  puts "Created #{restaurant.name}"
+
+  review = Review.new(
+    content: Faker::Superhero.name,
+    rating: rand(0..5)
+  )
+  review.restaurant = restaurant
+  review.save!
+end
